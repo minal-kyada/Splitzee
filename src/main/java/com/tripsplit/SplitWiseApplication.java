4aa5e090dd06392bd594f8592918797d7925cbc0
bhavikk <bhavikk.shah33@gmail.com>
2023-11-07 00:53:05 -0600

    Removed unsed import

--- src/main/java/com/tripsplit/SplitWiseApplication.java
+++ src/main/java/com/tripsplit/SplitWiseApplication.java
@@ -1,6 +1,5 @@
 package com.tripsplit;
 
-import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
