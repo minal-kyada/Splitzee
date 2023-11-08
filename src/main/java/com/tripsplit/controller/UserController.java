a755a929eb0777f997090d06465aa7ab5f20a712
bhavikk <bhavikk.shah33@gmail.com>
2023-11-07 19:46:18 -0600

    Exporting metrics to prometheus

--- src/main/java/com/tripsplit/controller/UserController.java
+++ src/main/java/com/tripsplit/controller/UserController.java
@@ -10,6 +10,8 @@ import com.tripsplit.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.*;
+import io.micrometer.core.annotation.Timed;
+
 
 import java.util.List;
 
@@ -20,11 +22,11 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-
     @Autowired
     private ApplicationEventPublisher publisher;
 
     @PostMapping("/register")
+    @Timed(value = "userRegister.time", description = "Time taken to register")
     public User registerUser(@RequestBody UserModel userModel){
 
         User user =userService.createUser(userModel);
@@ -37,26 +39,30 @@ public class UserController {
     }
     
     @PostMapping("/login")
+    @Timed(value = "userLogin.time", description = "Time taken to login")
     public User userLogin(@RequestBody UserLogin userLogin) throws Exception{
-
         return userService.userLogin(userLogin);
     }
     @GetMapping("/{id}")
+    @Timed(value = "fetchUserById.time", description = "Time taken to get user by id")
     public User getUserById(@PathVariable("id") Long userId){
         return userService.getUserById(userId);
     }
 
     @GetMapping("/all")
+    @Timed(value = "fetchAllUsers.time", description = "Time taken to fetch all users")
     public List<User> getAllUsers(){
         return userService.getAllUsers();
     }
 
     @GetMapping("/groups/{id}")
+    @Timed(value = "fetchGroupsById.time", description = "Time taken to get groups by id")
     public List<Group> getUserGroups(@PathVariable("id") Long userId){
         return userService.getUserGroups(userId);
     }
 
     @GetMapping("/expenses/{id}")
+    @Timed(value = "fetchExpenses.time", description = "Time taken to fetch expenses")
     public List<Expense> getUserExpenses(@PathVariable("id") Long userId){
         return userService.getUserExpenses(userId);
     }
@@ -64,6 +70,7 @@ public class UserController {
     
 
     @DeleteMapping("/{id}")
+    @Timed(value = "deleteUser.time", description = "Time taken to delete user")
     public String deleteUser(@PathVariable("id") Long userId){
         return userService.deleteUser(userId);
     }
